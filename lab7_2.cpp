e0904a51ce32c1009b3fc441fd03758e1c6883fb
Tonmokeiei <tonmoke2546@gmail.com>
2022-12-25 14:52:46 +0700

    lap7_2.cpp completed

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -1,14 +1,40 @@
 #include<iostream>
-
 using namespace std;
 
 int main(){
-	
+
 	int i = 0, P[5000], k, result;
 	while(i < 5000){
+
+	int i = 0; 
+	int P[5000]; 
+	int k,result;
+	cout<<"Please input k: ";
+	cin>>k;	
+	if(k>0)
+	{
+		result = 0;
+	while(i < 5000)
+	{	
 		P[i] = 2*i+1;
+		if( P[i]%k == 0)
+		{
+			result -=P[i];
+		}
+		else
+		{
+			result += P[i];
+		}
+
 		i++;
 	}
-	
+	cout<<"Result = "<<result;
+	}
+	else
+	{
+		cout<<"Invalid input!!!";
+	}
+
 	return 0;
+}
 }
