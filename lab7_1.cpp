e2dacfad254d374cb347d5b4c1cbdb118caad7c3
Tonmokeiei <tonmoke2546@gmail.com>
2022-12-25 14:46:15 +0700

    lap7_1.cpp completed

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -20,7 +20,7 @@ string func2(string x){
 		y += toupper(x[i]);
 		i++;
 	}
-	return y;	
+	return y;	 
 }
 
 string func3(string x){
@@ -29,13 +29,31 @@ string func3(string x){
 	while(i < L){
 		y += tolower(x[i]);
 		i++;
-	}
+	}	
 	return y;	
 }
 
+string Text1,Text2,Text3,Text4;
+
 int main(){
-    cout >> "Input text: "
-    cout >> "Reversed text: "
-    cout >> "Palindrome: "
+    
+	cout << "Input text: ";
+	cin >>Text1;
+	Text2 = func3(Text1);
+	Text3 = func1(Text2);
+	Text4 = func1(Text1);
+
+    cout << "Reversed text: ";
+	cout << (Text4)<<"\n";
+    cout << "Palindrome: ";
+	if(Text2 == Text3)
+	{
+		cout<<"Yes"<<"\n";
+	}
+	else
+	{
+		cout<<"No"<<"\n";
+	}
+
     return 0;
 }
